c0a676dfca6f81870cebd67ee4f68f0719a483ae
daly richy <dalyeu@yahoo.fr>
2020-03-17 21:18:02 -0400

    Added getSingleType method to the typeController

--- backend-api/src/main/java/org/wcci/apimastery/TypeController.java
+++ backend-api/src/main/java/org/wcci/apimastery/TypeController.java
@@ -1,10 +1,11 @@
 package org.wcci.apimastery;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+
+
 @RestController
 public class TypeController {
 
@@ -14,8 +15,14 @@ public class TypeController {
 
         this.typeRepository = typeRepository;
     }
- @RequestMapping("/types")
+
+ @GetMapping("/types")
     public Collection<Type> retrieveTypes() {
         return (Collection<Type>) typeRepository.findAll();
+    }
+
+    @GetMapping("/types/{id}")
+    public Type getSingleType(@PathVariable Long id){
+        return typeRepository.findById(id).get();
     }
 }
